05fa03e46fd6afa00a39414f1f7d4a5bf6105467
Rachel Cameron <rachleecameron@gmail.com>
2021-11-14 20:17:00 -0500

    Finalized Item Class + Updated Pseudocode

--- src/main/java/ucf/assignments/Item.java
+++ src/main/java/ucf/assignments/Item.java
@@ -9,23 +9,32 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
 public class Item {
+    //declaring item description
     private String description;
+    //declaring item due date (string to handle the proper "YYYY-MM-DD" format's dashes)
     private String dueDate;
+    //declaring completion status (boolean indicates if true/complete or false/incomplete)
     private boolean complete;
 
+    //constructing an item, made up of a description, due date, and completion status
     public Item(String description, String dueDate, boolean complete){
+        //adding item elements to item
         this.description = description;
         this.dueDate = dueDate;
         this.complete = complete;
     }
 
+    //sets the description
     public void setDescription(String description){
+        //will only set the description if valid
         if(descriptionIsValid(description)){
             this.description = description;
         }
     }
 
+    //sets the due date
     public void setDueDate(String dueDate){
+        //will only set the due date if valid
         if(dueDateIsValid(dueDate)){
             this.dueDate = dueDate;
         }
@@ -34,29 +43,35 @@ public class Item {
         }
     }
 
+    //sets the completion status
     public void setComplete(boolean complete){
         this.complete = complete;
     }
 
+    //gets item description
     public String getDescription(){
-        //used to get the description
         return description;
     }
 
+    //gets item due date
     public String getDueDate(){
         return dueDate;
     }
 
+    //gets item completion status
     public boolean getComplete(){
         return complete;
     }
 
+    //makes sure that the description is valid (between 1-256 characters and not an error message)
     public static boolean descriptionIsValid(String description){
         return description.length() >= 1 && description.length() <= 256 && !(description.equals("One or more invalid entries, please try again."));
     }
 
+    //makes sure the due date is valid by checking it against LocalDate to determine if within the Gregorian Calendar and formatted as YYYY-MM-DD
     public static boolean dueDateIsValid(String dueDate){
         try {
+            //i love LocalDate!
             LocalDate.parse((dueDate));
         } catch (DateTimeParseException e){
             return false;
